d3813562590a58c85010ca725d7770c366eaa62e
Sandun Sahiru <134186922+sandunsahiru@users.noreply.github.com>
2023-12-30 20:50:13 +0530

    products js change

--- src/main/webapp/AllProduct.js
+++ src/main/webapp/AllProduct.js
@@ -1,167 +1,171 @@
-const btnCart=document.querySelector('#cart-icon');
-const cart=document.querySelector('.cart');
-const btnClose=document.querySelector('#cart-close');
+const btnCart = document.querySelector('#cart-icon');
+const cart = document.querySelector('.cart');
+const btnClose = document.querySelector('#cart-close');
+
 
 var items = [
-  { name: 'Corn Cob 2Pcs', category: 'Vegetabales', price: 'Rs.180', imgSrc: 'images/image14.jpg' },
-  { name: 'Tomato 500g', category: 'Vegetabales', price: 'Rs.410', imgSrc: 'images/image17.jpg' },
-  { name: 'Kiwi', category: 'Fruits', price: 'Rs.320', imgSrc: 'images/image13.jpg' },
-  { name: 'Raththi Milk Powder 400g', category: 'Grocery', price: 'Rs.1240', imgSrc: 'images/image18.jpg' },
-  { name: 'Orange', category: 'Fruits', price: 'Rs.65', imgSrc: 'images/image11.jpg' },
-  { name: 'Anchor Butter 275g', category: 'Grocery', price: 'Rs.500', imgSrc: 'images/image19.jpg' },
-  { name: 'Top Crust Bread 450g', category: 'Bakery', price: 'Rs.150', imgSrc: 'images/image12.jpg' },
-  { name: 'Dinner Bun 3s', category: 'Bakery', price: 'Rs.170', imgSrc: 'images/image15.jpg' },
-  { name: 'carrot', category: 'Vegetabales', price: 'Rs.100', imgSrc: 'images/image6.jpg' },
-  { name: 'Pumpkin', category: 'Vegetabales', price: 'Rs.65', imgSrc: 'images/image20.jpg' },
-  { name: 'Red Apple', category: 'Fruits', price: 'Rs.80', imgSrc: 'images/image21.jpg' },
-  { name: 'Maliban Chocolate Biscuits', category: 'Grocery', price: 'Rs.250', imgSrc: 'images/image22.jpg' },
-  { name: 'Green Apple', category: 'Fruits', price: 'Rs.100', imgSrc: 'images/images23.jpg' },
-  { name: 'Araliya Supiri kiri samba', category: 'Grocery', price: 'Rs.1250', imgSrc: 'images/image24.jpg' },
-  { name: 'French Bread', category: 'Bakery', price: 'Rs.300', imgSrc: 'images/image16.jpg' },
-  { name: 'Kibula bun', category: 'Bakery', price: 'Rs.120', imgSrc: 'images/image8.jpg' },
-  { name: 'Green Beans 500g', category: 'Vegetabals', price: 'Rs.60', imgSrc: 'images/image25.jpg' },
-  { name: 'Snake Gourd', category: 'Vegetabales', price: 'Rs.65', imgSrc: 'images/image26.jpg' },
-  { name: 'Banana', category: 'Fruits', price: 'Rs.80', imgSrc: 'images/image27.jpg' },
-  { name: 'Coconut', category: 'Grocery', price: 'Rs.55', imgSrc: 'images/image28.jpg' },
-  { name: 'watermelon', category: 'Fruits', price: 'Rs.25', imgSrc: 'images/image29.jpg' },
-  { name: 'Coconut oil', category: 'Grocery', price: 'Rs.450', imgSrc: 'images/image30.jpg' },
-  { name: 'Egg Roti', category: 'Bakery', price: 'Rs.65', imgSrc: 'images/image9.jpg' },
-  { name: 'Fisg bun', category: 'Bakery', price: 'Rs.65', imgSrc: 'images/image31.jpg' }
+  { name: 'Pumpkin', category: 'Vegitabale', price: 'Rs.420', imgSrc: 'img/vegetables/pumpkin.jpeg' },
+  { name: 'Cauliflower 100g', category: 'Vegitabale', price: 'Rs.150.00', imgSrc: 'img/vegetables/cauliflower.jpeg' },
+  { name: 'Watermelon', category: 'Fruite', price: 'Rs.520.00', imgSrc: 'img/fruits/watermelon.jpeg' },
+  { name: 'Choco Grains 150g', category: 'Gosari', price: 'Rs.360.00', imgSrc: 'img/grocery/cereals-.png' },
+  { name: 'Papaya 1kg', category: 'Fruite', price: 'Rs.250.00', imgSrc: 'img/fruits/papaya.jpg' },
+  { name: 'Jumbo Peanuts 100g', category: 'Gosari', price: 'Rs.330.00', imgSrc: 'img/grocery/jumbo_peanuts-.png' },
+  { name: 'Dinner Bun 3s', category: 'Bakery', price: 'Rs.170.00', imgSrc: 'img/bakery/dinner-bun.jpg' },
+  { name: 'Top Crust Bread 450g', category: 'Bakery', price: 'Rs.150.00', imgSrc: 'img/bakery/crust-bread.jpg' },
+  { name: 'Kiwi 500g', category: 'Fruite', price: 'Rs.320.00', imgSrc: 'img/fruits/alimentarium_kiwis_0.jpg' },
+  { name: 'Bell Pepper 100g', category: 'Vegitabale', price: 'Rs.296.00', imgSrc: 'img/vegetables/bellpepper.png' },
+  { name: 'Lettuce 100g', category: 'Vegitabale', price: 'Rs.390.00', imgSrc: 'img/vegetables/lettuce.png' },
+  { name: 'Banana - Kolikuttu 100g', category: 'Fruite', price: 'Rs.70.00', imgSrc: 'img/fruits/Bananas.jpg' },
+  { name: 'Ratthi Milk Powder 400g', category: 'Gosari', price: 'Rs.1240.00', imgSrc: 'img/grocery/Raththi-.png' },
+  { name: 'Mangosteen 1kg', category: 'Fruite', price: 'Rs.350.00', imgSrc: 'img/fruits/Mangosteen.png' },
+  { name: 'Tomatoes 500g', category: 'Vegitabale', price: 'Rs.410.00', imgSrc: 'img/vegetables/Tomato_je.jpg' },
+  { name: 'Munchee Biscuit Kalo 90g', category: 'Gosari', price: 'Rs.100.00', imgSrc: 'img/grocery/Chocolate_Biscuit.png' },
+  { name: 'Corn Cob 2Pcs', category: 'Vegitabale', price: 'Rs.180.00', imgSrc: 'img/vegetables/CornCorb-scaled.jpg' },
+  { name: 'French Bread', category: 'Bakery', price: 'Rs.300.00', imgSrc: 'img/bakery/french-bread.jpg' },
+  { name: 'Happy Cow Cheese 120g', category: 'Gosari', price: 'Rs.736.00', imgSrc: 'img/grocery/happy_cow-.png' },
+  { name: 'Beetroot 300g', category: 'Vegitabale', price: 'Rs.264.00', imgSrc: 'img/vegetables/beetroot.png' },
+  { name: 'Pomegranate 300g', category: 'Fruite', price: 'Rs.590.00', imgSrc: 'img/fruits/Pomegranate.png' },
+  { name: 'Uswatte Tipi Tip Onion Stars 25g', category: 'Gosari', price: 'Rs.100.00', imgSrc: 'img/grocery/tipi_tip-.png' },
+  { name: 'Guava 100g', category: 'Fruite', price: 'Rs.85.00', imgSrc: 'img/fruits/Guava.png' },
+  { name: 'Kandos Cashew Nut Chocolate 40g', category: 'Gosari', price: 'Rs.220.00', imgSrc: 'img/grocery/Cashew_Nut_100g-.png' },
+  { name: 'Brinjals 300g', category: 'Vegitabale', price: 'Rs.240.00', imgSrc: 'img/vegetables/eggplants.png' },
+  { name: 'Samaposha Cereal 200g', category: 'Gosari', price: 'Rs.175.00', imgSrc: 'img/grocery/samaposha-.png' },
+  { name: 'Mango 100g', category: 'Fruite', price: 'Rs.65.00', imgSrc: 'img/fruits/mango.png' }
 ];
 
-document.addEventListener('DOMContentLoaded', function() {
-  
+document.addEventListener('DOMContentLoaded', function () {
+
   function filterItems(category) {
     var shopContent = document.getElementById('shopContent');
- 
-      items.forEach(function(item) {
-          var foodBox = document.createElement('div');
-          foodBox.className = 'food-box';
- 
-          if (category === 'All' || item.category.includes(category)) {
-              var picDiv = document.createElement('div');
-              picDiv.className = 'pic ' + item.category;
-              var img = document.createElement('img');
-              img.src = item.imgSrc;
-              img.className = 'food-img';
-              picDiv.appendChild(img);
 
-              var h2 = document.createElement('h2');
-              h2.className = 'food-title';
-              h2.textContent = item.name;
+    items.forEach(function (item) {
+      var foodBox = document.createElement('div');
+      foodBox.className = 'food-box';
 
-              var span = document.createElement('span');
-              span.className = 'food-price';
-              span.textContent = item.price;
+      if (category === 'All' || item.category.includes(category)) {
+        var picDiv = document.createElement('div');
+        picDiv.className = 'pic ' + item.category;
+        var img = document.createElement('img');
+        img.src = item.imgSrc;
+        img.className = 'food-img';
+        picDiv.appendChild(img);
 
-              var ionIcon = document.createElement('ion-icon');
-              ionIcon.className = 'add-cart';
-              ionIcon.setAttribute('name', 'cart');
+        var h2 = document.createElement('h2');
+        h2.className = 'food-title';
+        h2.textContent = item.name;
 
-              foodBox.appendChild(picDiv);
-              foodBox.appendChild(h2);
-              foodBox.appendChild(span);
-              foodBox.appendChild(ionIcon);
+        var span = document.createElement('span');
+        span.className = 'food-price';
+        span.textContent = item.price;
 
-              shopContent.appendChild(foodBox);
-              loadContent()
-          }
-      });
+        var ionIcon = document.createElement('ion-icon');
+        ionIcon.className = 'add-cart';
+        ionIcon.setAttribute('name', 'cart');
+
+        foodBox.appendChild(picDiv);
+        foodBox.appendChild(h2);
+        foodBox.appendChild(span);
+        foodBox.appendChild(ionIcon);
+
+        shopContent.appendChild(foodBox);
+        loadContent()
+      }
+    });
   }
- 
+
   var filterButtons = document.querySelectorAll('.filter-btn');
-  filterButtons.forEach(function(button) {
-      button.addEventListener('click', function() { 
-          shopContent.innerHTML = ''; 
-          filterItems(this.id);
-      });
+  filterButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      shopContent.innerHTML = '';
+      filterItems(this.id);
+    });
   });
- 
+
   filterItems('All');
 });
 
-btnCart.addEventListener('click',()=>{
+btnCart.addEventListener('click', () => {
   cart.classList.add('cart-active');
 });
 
-btnClose.addEventListener('click',()=>{
+btnClose.addEventListener('click', () => {
   cart.classList.remove('cart-active');
 });
 
-document.addEventListener('DOMContentLoaded',loadFood);
+document.addEventListener('DOMContentLoaded', loadFood);
 
-function loadFood(){
+function loadFood() {
   loadContent();
 
 }
 
-function loadContent(){ 
-  let btnRemove=document.querySelectorAll('.cart-remove');
-  btnRemove.forEach((btn)=>{
-    btn.addEventListener('click',removeItem);
+function loadContent() {
+  let btnRemove = document.querySelectorAll('.cart-remove');
+  btnRemove.forEach((btn) => {
+    btn.addEventListener('click', removeItem);
   });
- 
-  let qtyElements=document.querySelectorAll('.cart-quantity');
-  qtyElements.forEach((input)=>{
-    input.addEventListener('change',changeQty);
+
+  let qtyElements = document.querySelectorAll('.cart-quantity');
+  qtyElements.forEach((input) => {
+    input.addEventListener('change', changeQty);
   });
- 
-  let cartBtns=document.querySelectorAll('.add-cart');
-  cartBtns.forEach((btn)=>{
+
+  let cartBtns = document.querySelectorAll('.add-cart');
+  cartBtns.forEach((btn) => {
     btn.addEventListener('click', addCart);
   });
 
   updateTotal();
 }
 
- 
-function removeItem(){
-  if(confirm('Are Your Sure to Remove')){
-    let title=this.parentElement.querySelector('.cart-food-title').innerHTML;
-    itemList=itemList.filter(el=>el.title!=title);
+
+function removeItem() {
+  if (confirm('Are Your Sure to Remove')) {
+    let title = this.parentElement.querySelector('.cart-food-title').innerHTML;
+    itemList = itemList.filter(el => el.title != title);
     this.parentElement.remove();
     loadContent();
   }
 }
- 
-function changeQty(){
-  if(isNaN(this.value) || this.value<1){
-    this.value=1;
+
+function changeQty() {
+  if (isNaN(this.value) || this.value < 1) {
+    this.value = 1;
   }
   loadContent();
 }
 
-let itemList=[];
+let itemList = [];
 
 //Add Cart
-function addCart(){ 
- let food=this.parentElement;
- let title=food.querySelector('.food-title').innerHTML;
- let price=food.querySelector('.food-price').innerHTML;
- let imgSrc=food.querySelector('.food-img').src;
- //console.log(title,price,imgSrc);
- 
- let newProduct={title,price,imgSrc}
+function addCart() {
+  let food = this.parentElement;
+  let title = food.querySelector('.food-title').innerHTML;
+  let price = food.querySelector('.food-price').innerHTML;
+  let imgSrc = food.querySelector('.food-img').src;
+  //console.log(title,price,imgSrc);
 
- //Check Product already Exist in Cart
- if(itemList.find((el)=>el.title==newProduct.title)){
-  alert("Product Already added in Cart");
-  return;
- }else{
-  itemList.push(newProduct);
- }
+  let newProduct = { title, price, imgSrc }
+
+  //Check Product already Exist in Cart
+  if (itemList.find((el) => el.title == newProduct.title)) {
+    alert("Product Already added in Cart");
+    return;
+  } else {
+    itemList.push(newProduct);
+  }
 
 
-let newProductElement= createCartProduct(title,price,imgSrc);
-let element=document.createElement('div');
-element.innerHTML=newProductElement;
-let cartBasket=document.querySelector('.cart-content');
-cartBasket.append(element);
-loadContent();
+  let newProductElement = createCartProduct(title, price, imgSrc);
+  let element = document.createElement('div');
+  element.innerHTML = newProductElement;
+  let cartBasket = document.querySelector('.cart-content');
+  cartBasket.append(element);
+  loadContent();
 }
 
 
-function createCartProduct(title,price,imgSrc){
+function createCartProduct(title, price, imgSrc) {
 
   return `
   <div class="cart-box">
@@ -179,59 +183,70 @@ function createCartProduct(title,price,imgSrc){
   `;
 }
 
-function updateTotal()
-{
-  const cartItems=document.querySelectorAll('.cart-box');
-  const totalValue=document.querySelector('.total-price');
+function updateTotal() {
+  const cartItems = document.querySelectorAll('.cart-box');
+  const totalValue = document.querySelector('.total-price');
 
-  let total=0;
+  let total = 0;
 
-  cartItems.forEach(product=>{
-    let priceElement=product.querySelector('.cart-price');
-    let price=parseFloat(priceElement.innerHTML.replace("Rs.",""));
-    let qty=product.querySelector('.cart-quantity').value;
-    total+=(price*qty);
-    product.querySelector('.cart-amt').innerText="Rs."+(price*qty);
+  cartItems.forEach(product => {
+    let priceElement = product.querySelector('.cart-price');
+    let price = parseFloat(priceElement.innerHTML.replace("Rs.", ""));
+    let qty = product.querySelector('.cart-quantity').value;
+    total += (price * qty);
+    product.querySelector('.cart-amt').innerText = "Rs." + (price * qty);
 
   });
 
-  totalValue.innerHTML='Rs.'+total;
+  totalValue.innerHTML = 'Rs.' + total;
 
 
   // Add Product Count in Cart Icon
 
-  const cartCount=document.querySelector('.cart-count');
-  let count=itemList.length;
-  cartCount.innerHTML=count;
+  const cartCount = document.querySelector('.cart-count');
+  let count = itemList.length;
+  cartCount.innerHTML = count;
 
-  if(count==0){
-    cartCount.style.display='none';
-  }else{
-    cartCount.style.display='block';
+  if (count == 0) {
+    cartCount.style.display = 'none';
+  } else {
+    cartCount.style.display = 'block';
   }
-
-
 }
-document.querySelector('.btn-buy').addEventListener('click', function(event) {
-    // Prevent the default form submission
-    event.preventDefault();
 
-    // Collect the cart data
-    let cartItems = [];
-    document.querySelectorAll('.cart-box').forEach(item => {
-        let title = item.querySelector('.cart-food-title').innerText;
-        let price = item.querySelector('.cart-price').innerText;
-        let qty = item.querySelector('.cart-quantity').value;
-        let imgSrc = item.querySelector('.cart-img').src;
 
-        cartItems.push({ title, price, qty, imgSrc });
+
+
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+  searchForm.classList.toggle('active');
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var loginBtn = document.getElementById("login-btn");
+
+  loginBtn.addEventListener("click", function () {
+    window.location.href = "login-page.html";
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const slider = document.querySelector('.slider');
+  const images = document.querySelectorAll('.slider img');
+
+  let currentSlide = 0;
+
+  function showNextSlide() {
+    currentSlide = (currentSlide + 1) % images.length;
+    const offset = -currentSlide * 100;
+    images.forEach((img, index) => {
+      img.style.transition = `transform 1.5s ${index * 0}s ease-in-out`;
+      img.style.transform = `translateX(${offset}%)`;
     });
-
-    // Convert cart data to JSON and assign to the hidden input
-    document.getElementById('orderData').value = JSON.stringify(cartItems);
-
-    // Submit the form
-    document.getElementById('orderForm').submit();
+  }
+  setInterval(showNextSlide, 5000);
 });
 
 
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -7,49 +7,71 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Green Online</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="login.css">
-</head>
-<body>
-    <div class="wrapper">
-        <h1>Login</h1>
-        <p>Shopped with us before?<br>Login with your credentials.</p>
-        <div>
-            <p class="new-memb">New Member?</p>
-            <a href="signup.jsp"><button type="submit" class="reg-btn">Click here to Register!</button></a>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Green Online</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        <link rel="stylesheet" href="login.css">
+        <style>
+            .header .logo {
+                font-size: 2.5rem;
+                font-weight: bolder;
+                color: #293326;
+            }
+            .header {
+                position: fixed;
+                top: 0;
+                left: 0;
+                right: 0;
+                z-index: 1000;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                padding: 2rem 9%;
+                margin-bottom: 5px;
+            }
+        </style>
+    </head>
+    <body>
+        <header class="header">
+            <a href="home-page.html" class="logo"><i class="fas fa-shopping-basket"></i>Greenie</a>
+        </header>
+        <div class="wrapper">
+            <h1>Login</h1>
+            <p>Shopped with us before?<br>Login with your credentials.</p>
+            <div>
+                <p class="new-memb">New Member?</p>
+                <a href="signup.jsp"><button type="submit" class="reg-btn">Click here to Register!</button></a>
+            </div>
+            <div class="trouble">
+                <p>Have trouble logging in?<br>Call us on <a href="#">0112230125</a><br>(Daily Operating Hours 8.00 a.m<br>
+                    to 8.00 p.m)</p>
+            </div>
         </div>
-        <div class="trouble">
-            <p>Have trouble logging in?<br>Call us on <a href="#">0112230125</a><br>(Daily Operating Hours 8.00 a.m<br>
-                to 8.00 p.m)</p>
-        </div>
-    </div>
-    <form action="LoginServlet" method="post">
-    <div class="wrapper2">
-        <div class="sec-1">
-            <div class="input-box">
-                <h3>Email</h3>
-                <input type="text" name="email" placeholder="Email" required>
-                <div class="tip">
-                    <i class="fa-solid fa-circle-info"></i>
-                    <p class="infoP">New here? Sign up with your email/phone number to get started!</p>
+        <form action="LoginServlet" method="post">
+            <div class="wrapper2">
+                <div class="sec-1">
+                    <div class="input-box">
+                        <h3>Email</h3>
+                        <input type="text" name="email" placeholder="Email" required>
+                        <div class="tip">
+                            <i class="fa-solid fa-circle-info"></i>
+                            <p class="infoP">New here? Sign up with your email/phone number to get started!</p>
+                        </div>
+                    </div>
+                    <div class="input-box">
+                        <h3>Password</h3>
+                        <input type="password" name="password" placeholder="Password" required>
+                        <i class="fa-regular"></i>
+                        <div class="forg"><a href="#">Forgot Password?</a></div>
+                    </div>
                 </div>
+                <button type="submit" class="log-btn">LOGIN</button>
             </div>
-            <div class="input-box">
-                <h3>Password</h3>
-                <input type="password" name="password" placeholder="Password" required>
-                <i class="fa-regular"></i>
-                <div class="forg"><a href="#">Forgot Password?</a></div>
-            </div>
-        </div>
-        <button type="submit" class="log-btn">LOGIN</button>
-    </div>
         </form>
-        
-    <script src="js/login.js"></script>
-</body>
+
+        <script src="js/login.js"></script>
+    </body>
 </html>
 
